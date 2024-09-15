0fba11899b468081cd4f5de7d8e6e88a24a3f8ed
abhay <abhayforwork02@gmail.com>
2024-09-15 17:38:00 +0530

    css changed

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -7,35 +7,35 @@ import logo from '../Assets/Radiant logo mark and type.png'
 export const Login = () => {
   return (
     <>
-    <div className='logo-img'>
-        <img src={logo} alt="logo" />
+    <div class='logo-img'>
+        <img src={logo} alt="logo" height={60} width={280}  />
       </div>
-    <div className='main'>
+    <div class='main'>
       <div className='container'>
-        <div className='header'>
-            <div className='text'>Login</div>
+        <div class='header'>
+            <div class='text'>Login</div>
         </div>
       </div>
-      <div className='icons'>
-        <div className='g-img'>
-            <button><img className='img1' src={google} alt='google'/></button>
-            <button><img className='img2' src={In} alt='Linkedin'/></button>
+      <div class='icons'>
+        <div class='g-img'>
+            <button><img class='img1' src={google} alt='google'/></button>
+            <button><img class='img2' src={In} alt='Linkedin'/></button>
         </div>
        </div>
-       <div className='or'>or</div>
-       <div className='inputs'>
-        <div className='input'>
+       <div class='or'>or</div>
+       <div class='inputs'>
+        <div class='input'>
             <input type='text'/>
         </div>
-        <div className='input'>
+        <div class='input'>
             <input type='password'/>
         </div>
-        <div className='submit-container'>
-            <div className='submit'>Log in</div>
+        <div class='submit-container'>
+            <div class='submit'>Log in</div>
         </div>
-        <div className='others'>
-        <div className='reset'>Reset Password</div>
-        <div className='signup'>New user? <span>Sign Up</span></div>
+        <div class='others'>
+        <div class='reset'>Reset Password</div>
+        <div class='signup'>New user? <span>Sign Up</span></div>
         </div>
        </div>
     </div>
